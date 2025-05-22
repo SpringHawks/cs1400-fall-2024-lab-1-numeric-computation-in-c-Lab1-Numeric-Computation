e5f6f401df744323cc9420f4b4b178b64e4dec9a
Annette Hawks <hawksannette@gmail.com>
2025-05-21 21:19:11 -0600

    Code 8 = 3

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿//Lab 1 
 //Numerical Computations 
 
-int a = 5;
+int a = 7;
 int b = 4;
-int c = 2;
-int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+int c = 3;
+int d = (a + b) / c;
 Console.WriteLine(d);
-//  = 25
+//  = 3
