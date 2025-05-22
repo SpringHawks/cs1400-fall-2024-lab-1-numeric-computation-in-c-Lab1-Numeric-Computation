87fc2739a0ccad6ebb0a955d33b85c599f1cd586
Annette Hawks <hawksannette@gmail.com>
2025-05-21 21:23:01 -0600

    Code 11

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿//Lab 1 
 //Numerical Computations 
-
 int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
-//The range of integers is -2147483648 to 2147483647
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+
+// The range of integers is -2147483648 to 2147483647
+//An example of overflow: -2147483646
