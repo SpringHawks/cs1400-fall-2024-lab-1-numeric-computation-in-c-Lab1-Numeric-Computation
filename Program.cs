7794ba72ac9f22fdd4a1ab2f13c0f5a826c37830
Annette Hawks <hawksannette@gmail.com>
2025-05-21 21:20:06 -0600

    Code 9 = Q3 and R2

--- Program.cs
+++ Program.cs
@@ -5,5 +5,8 @@ int a = 7;
 int b = 4;
 int c = 3;
 int d = (a + b) / c;
-Console.WriteLine(d);
-//  = 3
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+//quotient: 3
+//remainder: 2
